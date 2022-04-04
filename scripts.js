f271216810a0080025332e0415573d451ffe8556
azvdu <gabriel.liradefarias2@gmail.com>
2022-04-04 13:17:28 -0300

    selecao completa

--- scripts.js
+++ scripts.js
@@ -1,24 +1,22 @@
 function opcaoPrato (prato) {
-    let selecionado = document.querySelector(".prato .selecionado");
-    // prato.classList.toggle("selecionado");
-    if (selecionado !== null) {
-        selecionado.classList.remove("selecionado")
+    let escolha = document.querySelector(".pratos .selecionado");
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado")
     }
     prato.classList.add("selecionado")
 }
 function opcaoBebida (bebida) {
-    let selecionado = document.querySelector(".bebida .selecionado");
-    // prato.classList.toggle("selecionado");
-    if (selecionado !== null) {
-        selecionado.classList.remove("selecionado")
+    let escolha = document.querySelector(".bebidas .selecionado");
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado")
     }
     bebida.classList.add("selecionado")
 }
 function opcaoSobremesa (sobremesa) {
-    let selecionado = document.querySelector(".sobremesa .selecionado");
-    // prato.classList.toggle("selecionado");
-    if (selecionado !== null) {
-        selecionado.classList.remove("selecionado")
+    let escolha = document.querySelector(".sobremesa .selecionado");
+    if (escolha !== null) {
+        escolha.classList.remove("selecionado")
     }
     sobremesa.classList.add("selecionado")
 }
+
