2fe6ecc30d1d159320e4b854f3986f307bf2eb7d
azvdu <gabriel.liradefarias2@gmail.com>
2022-04-04 12:36:42 -0300

    seleção feita

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
-function opcaoSelecionada (opcaoEscolhida) {
-    opcaoEscolhida.classLista.toggle("selecionado");
+function opcaoPrato (prato) {
+    let selecionado = document.querySelector(".prato .selecionado");
+    // prato.classList.toggle("selecionado");
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado")
+    }
+    prato.classList.add("selecionado")
+}
+function opcaoBebida (bebida) {
+    let selecionado = document.querySelector(".bebida .selecionado");
+    // prato.classList.toggle("selecionado");
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado")
+    }
+    bebida.classList.add("selecionado")
+}
+function opcaoSobremesa (sobremesa) {
+    let selecionado = document.querySelector(".sobremesa .selecionado");
+    // prato.classList.toggle("selecionado");
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado")
+    }
+    sobremesa.classList.add("selecionado")
 }
